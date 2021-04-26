9f10ebe132a46790e6f19679261e84e305666344
ujjwal-git <ind.ujjwalkumar2013@gmail.com>
2021-04-26 11:44:08 +0530

    computation of power of a no. using recursion

---
+++ pow-rec.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int power_fun(int , int);
+
+int main(void){
+    int power = 0;
+    int base, exponent;
+
+    printf("This program prints power of a no. \n");
+
+    printf("Enter base no.: \n");
+    scanf("%d \n", &base);
+
+    printf("Enter exponent of base: \n");
+    scanf("%d \n", &exponent);
+
+    power = power_fun(base , exponent);
+
+    printf("Here is the power of %d raised to %d = \n", base, exponent, power);
+
+}
+
+int power_fun(int b , int e){
+
+    if(!e) return 1;
+    else if(e > 0){
+        return b * power_fun(b , --e);
+    }
+}
